9265a656a91fdc7fb9bc738f1dec3005a06f7e74
stefansiebers <s3f3s-github@yahoo.com>
2024-08-28 17:02:47 +0200

    add deposit and withdrawal

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -11,4 +11,21 @@ public class Account {
         accountNumber = client.firstName().substring(0,1).toUpperCase() + client.lastName().substring(0,1) + counter++;
         balance = BigDecimal.ZERO;
     }
+
+    public void deposit(BigDecimal amount) {
+        balance = balance.add(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                ", client=" + client +
+                '}';
+    }
+
+    public void withdraw(BigDecimal amount) {
+        balance = balance.subtract(amount);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,13 @@
+import java.math.BigDecimal;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Client kurt = new Client("Kurt", "Schulze", 2);
+        Account kurtsAccount = new Account(kurt);
+        System.out.println(kurtsAccount);
+        kurtsAccount.deposit(new BigDecimal(270));
+        System.out.println(kurtsAccount);
+        kurtsAccount.withdraw(new BigDecimal(170));
+        System.out.println(kurtsAccount);
+    }
 }
